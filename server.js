560e2ae8b92020e80c661d7c2291cfce2b0bf43c
ad-world <67097683+ad-world@users.noreply.github.com>
2022-10-20 15:09:41 -0400

    Add public route for static files

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+// Set views path
+app.set('views', path.join(__dirname, 'views'));
+// Set public path
+app.set(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
 
