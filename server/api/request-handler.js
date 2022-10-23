cdf408ab102510f9f520a18eb4c861b14b3738d3
River Stanley <river.stanley173@gmail.com>
2022-10-23 18:17:52 -0400

    Feed GET requests into queue

--- server/api/request-handler.js
+++ server/api/request-handler.js
@@ -5,29 +5,11 @@ class RequestHandler {
   static queues = {};
 
   static getJSONData(req, res) {
-    try {
-      fs.readFile(`server/data/${req.params.fileName}.json`, (err, data) => {
-        try {
-          if (this._handleErrors(res, err)) return;
-
-          if (!data || data.length === 0) {
-            res.status(204).end(); // success, no data
-            return;
-          }
-
-          const parsedJson = JSON.parse(data.toString('utf-8'));
-          res.json(parsedJson);
-
-          res.status(200).end();
-        } catch (e) {
-          Logger.warn(e);
-          res.status(500).end();
-        }
-      });
-    } catch (e) {
-      Logger.warn(e);
-      res.status(500).end();
+    if (!this.queues[req.params.fileName]) {
+      this.queues[req.params.fileName] = new RequestQueue();
     }
+
+    this.queues[req.params.fileName].push(req, res, 'r');
   }
 
   static overwriteJSONData(req, res) {
@@ -35,42 +17,68 @@ class RequestHandler {
       this.queues[req.params.fileName] = new RequestQueue();
     }
 
-    this.queues[req.params.fileName].push(req, res);
+    this.queues[req.params.fileName].push(req, res, 'w');
   }
 
-  static _processJSONOverwriteQueue({ req, res }) {
+  static _processJSONReadQueue(requestData) {
+    try {
+      fs.readFile(`server/data/${requestData.req.params.fileName}.json`, (err, data) => {
+        try {
+          if (this._handleErrors(requestData.res, err)) return;
+
+          if (!data || data.length === 0) {
+            requestData.res.status(204).end(); // success, no data
+            return;
+          }
+
+          const parsedJson = JSON.parse(data.toString('utf-8'));
+          requestData.res.json(parsedJson);
+
+          requestData.res.status(200).end();
+        } catch (e) {
+          Logger.warn(e);
+          requestData.res.status(500).end();
+        }
+      });
+    } catch (e) {
+      Logger.warn(e);
+      requestData.res.status(500).end();
+    }
+  }
+
+  static _processJSONOverwriteQueue(requestData) {
     try {
       let writeData;
 
       try {
-        writeData = typeof req.body === "object"
-          ? JSON.stringify(req.body)
-          : req.body;
+        writeData = typeof requestData.req.body === "object"
+          ? JSON.stringify(requestData.req.body)
+          : requestData.req.body;
       } catch (e) {
-        res.status(400).end(); // bad input (probably cannot parse to JSON)
+        requestData.res.status(400).end(); // bad input (probably cannot parse to JSON)
       }
 
-      fs.open(`server/data/${req.params.fileName}.json`,
+      fs.open(`server/data/${requestData.req.params.fileName}.json`,
         'r+',
         null,
         (err) => {
-          if (this._handleErrors(res, err)) return;
+          if (this._handleErrors(requestData.res, err)) return;
 
           fs.writeFile(
-            `server/data/${req.params.fileName}.json`,
+            `server/data/${requestData.req.params.fileName}.json`,
             writeData,
             { encoding: 'utf8', flag: 'w' },
             (err, data) => {
-              if (this._handleErrors(res, err)) return;
+              if (this._handleErrors(requestData.res, err)) return;
 
-              res.status(200).end();
+              requestData.res.status(200).end();
             }
           );
         }
       );
     } catch (e) {
       Logger.warn(e);
-      res.status(500).end();
+      requestData.res.status(500).end();
     }
   }
 
@@ -89,15 +97,23 @@ class RequestHandler {
 class RequestQueue {
   _queue = [];
 
-  push(req, res) {
+  push(req, res, type) {
     if (this._queue.length === 0) {
-      this._queue.push({ req, res });
+      this._queue.push({ req: req, res: res, type: type });
       this.process();
-    } else this._queue.push({ req, res });
+    } else this._queue.push({ req: req, res: res, type: type });
   }
 
   process() {
-    RequestHandler._processJSONOverwriteQueue(this._queue[0]);
+    if (this._queue[0].type === 'r')
+      RequestHandler._processJSONReadQueue(this._queue[0]);
+    else if (this._queue[0].type === 'w')
+      RequestHandler._processJSONOverwriteQueue(this._queue[0]);
+    else {
+      Logger.warn(new Error('Queued request has bad type'))
+      this._queue[0].res.status(500).end();
+    }
+
     this._queue.splice(0, 1);
 
     if (this._queue.length > 0)
