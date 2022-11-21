83d32a3626684ef2889c179c5fd31298c56bbe6f
ad-world <67097683+ad-world@users.noreply.github.com>
2022-11-20 21:17:06 -0500

    Created home page (finally)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const navItems = require('../data/navbar.json');
+const footer = require('../data/footer.json');
 
 router.get('/', async (req, res) => {
-    res.render('pages/home');
+    res.render('pages/home', {navItems: navItems, footer: footer});
 })
 
 module.exports = router;
