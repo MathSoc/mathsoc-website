8df65603f49a497ba2747aeb7a6c9774925e6cd6
errorline99 <100209216+errorline99@users.noreply.github.com>
2022-12-02 03:07:08 +0300

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,13 @@
-/* JS code to be rendered to the web browser */
+var Btn = document.getElementById('Btn');
+var more = document.getElementById('more');
+var dots = document.getElementById('dots');
+
+function Readmore(){
+    if(more.style.display === "none"){
+        more.style.display = 'inline'
+    } else {
+        more.style.display = "none"
+    }
+}
+
+Btn.addEventListener('click', Readmore)
