72ba161fb6334d622fe4d4e47a5c1b21f1be3741
River Stanley <river.stanley173@gmail.com>
2022-12-13 23:49:10 -0500

    2022-12-13: Elections page almost complete, no CMS

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const navItems = require('../config/navbar.json');
 const footer = require('../config/footer.json');
+const electionsData = require('../data/elections.json');
 
 router.get('/', async (req, res) => {
     res.render('pages/home', {navItems: navItems, footer: footer});
 });
 
 router.get('/elections', async (req, res) => {
-  res.render('pages/elections/elections', {navItems: navItems, footer: footer});
+  res.render('pages/elections/elections', {navItems: navItems, footer: footer, elections: electionsData});
 })
 
 module.exports = router;
