78e173f7277d3cc575dbe05cf6b561272f5faa16
ad-world <67097683+ad-world@users.noreply.github.com>
2022-12-18 18:20:41 -0500

    Created wellness copy

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const navItems = require('../config/navbar.json');
 const footer = require('../config/footer.json');
 const electionsData = require('../data/elections.json');
+const wellnessData = require('../data/wellness.json');
 
 router.get('/', async (req, res) => {
     res.render('pages/home', {navItems: navItems, footer: footer});
@@ -9,6 +10,10 @@ router.get('/', async (req, res) => {
 
 router.get('/elections', async (req, res) => {
   res.render('pages/elections/elections', {navItems: navItems, footer: footer, elections: electionsData});
+})
+
+router.get('/mental-wellness', async (req, res) => {
+  res.render('pages/resources/mental-wellness', {navItems: navItems, footer: footer, wellness: wellnessData});
 })
 
 module.exports = router;
