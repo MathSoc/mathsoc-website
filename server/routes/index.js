9d328aabc539a98f5da6b8fc4dbb5e549416d8db
River Stanley <river.stanley173@gmail.com>
2022-12-21 20:07:28 -0500

    Contact us: build majority of frontend

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const navItems = require('../config/navbar.json');
 const footer = require('../config/footer.json');
 const electionsData = require('../data/elections.json');
+const execsData = require('../data/execs.json');
 
 router.get('/', async (req, res) => {
     res.render('pages/home', {navItems: navItems, footer: footer});
@@ -9,6 +10,10 @@ router.get('/', async (req, res) => {
 
 router.get('/elections', async (req, res) => {
   res.render('pages/elections/elections', {navItems: navItems, footer: footer, elections: electionsData});
-})
+});
+
+router.get('/contact-us', async (req, res) => {
+  res.render('pages/contact-us', {navItems: navItems, footer: footer, staff: execsData });
+});
 
 module.exports = router;
