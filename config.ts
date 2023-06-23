62a0ab055a9743f647c3688198497935886d8832
River Stanley <river.stanley173@gmail.com>
2023-06-23 17:55:26 -0400

    Make .env usage less obstructive

--- config.ts
+++ config.ts
@@ -1,27 +1,44 @@
 import "dotenv/config";
 
-const tokens = {
+// Will always prevent server initialization if this token is absent
+const requiredTokens = {
+  IS_DEVELOPMENT: process.env.IS_DEVELOPMENT,
+};
+
+// Will only prevent server initialization on prod/staging
+const requiredForProdTokens = {
   EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET,
   WATERLOO_OPEN_API_BASE_URL: process.env.WATERLOO_OPEN_API_BASE_URL,
   WATERLOO_OPEN_API_KEY: process.env.WATERLOO_OPEN_API_KEY,
+};
+
+// Will only prevent server initialization on prod/staging
+const authTokens = {
   ADFS_SERVER: process.env.ADFS_SERVER,
   ADFS_CLIENT_ID: process.env.ADFS_CLIENT_ID,
   REDIRECT_URI: process.env.REDIRECT_URI,
   COOKIE_ENCRYPTION_KEY: process.env.COOKIE_ENCRYPTION_KEY,
   COOKIE_ENCRYPTION_IV: process.env.COOKIE_ENCRYPTION_IV,
   POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI,
-  IS_DEVELOPMENT: process.env.IS_DEVELOPMENT,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_AUTH_SUCCESS_REDIRECT: process.env.GOOGLE_AUTH_SUCCESS_REDIRECT
+  GOOGLE_AUTH_SUCCESS_REDIRECT: process.env.GOOGLE_AUTH_SUCCESS_REDIRECT,
 };
 
 const missingTokens: string[] = [];
 
-for (const [key, value] of Object.entries(tokens)) {
-  if (!value) {
-    missingTokens.push(key);
+const catchEmptyVariables = (tokenMap: Record<string, any>) => {
+  for (const [key, value] of Object.entries(tokenMap)) {
+    if (!value) {
+      missingTokens.push(key);
+    }
   }
+};
+
+catchEmptyVariables(requiredTokens);
+if (requiredTokens.IS_DEVELOPMENT !== "true") {
+  catchEmptyVariables(authTokens);
+  catchEmptyVariables(requiredForProdTokens);
 }
 
 if (missingTokens.length) {
@@ -32,4 +49,4 @@ if (missingTokens.length) {
   process.exit(1);
 }
 
-export default tokens;
+export default { ...requiredTokens, ...requiredForProdTokens, ...authTokens };
