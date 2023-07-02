e1993b2554105cca861b2a2b693d8732b120e836
maxtan84 <mthockey84@gmail.com>
2023-07-02 17:12:53 -0400

    set up backend and success page if needed

---
+++ server/api/controllers/locker-signout-controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { Logger } from "../../util/logger";
+import { ReadWriteAPIController } from "./read-write-api-controller";
+import { ReadWriteController } from "./read-write-controller";
+import LockerData from "../../data/_hidden/locker-list.json";
+
+/* Assuming lockers.json will have the following structure: 
+{
+    {
+        "userid": "userid",    
+        "lockernumber": "lockerid",
+        "lockercombination": "lockercombination",
+    }
+    ...
+}
+*/
+export class LockerSignoutController {
+    static logger = new Logger();
+    
+    
+    static requestLocker(req: Request, res: Response) {
+        
+    }
+}
