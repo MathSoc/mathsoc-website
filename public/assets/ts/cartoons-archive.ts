0f7736600ed8af021a31e6d63f0d93ee15a7ed36
River Stanley <river.stanley@uwaterloo.ca>
2023-07-18 13:19:54 -0400

    Cartoons Archive: Frontend (#176)

---
+++ public/assets/ts/cartoons-archive.ts
@@ -0,0 +1,62 @@
+class CartoonsArchive {
+  static init() {
+    const courseButtons: HTMLButtonElement[] = Array.from(
+      document.querySelectorAll("button.course-button")
+    );
+
+    // register button listeners
+    for (const button of courseButtons) {
+      button.addEventListener("click", () => {
+        CartoonsArchive.openModalForButton(button);
+      });
+    }
+
+    // register close modal listeners
+    document
+      .getElementById("cartoon-display-bg")
+      .addEventListener("click", CartoonsArchive.closeModal);
+    document
+      .getElementById("cartoon-display-modal") // prevent clicking the modal from closing it
+      .addEventListener("click", (e: MouseEvent) => e.stopPropagation());
+    document
+      .getElementById("cartoons-modal-close-btn")
+      .addEventListener("click", CartoonsArchive.closeModal);
+  }
+
+  private static closeModal() {
+    document.getElementById("cartoon-display-bg").classList.remove("active");
+  }
+
+  /**
+   * Handles opening the cartoon display modal when a cartoon button is clicked
+   */
+  private static openModalForButton(button: HTMLButtonElement) {
+    const subject = button.getAttribute("data-subject");
+    const courseCode = button.getAttribute("data-course-code");
+    const cartoonDirectory = button.getAttribute("data-cartoon-dirname");
+    const imageSources = button.getAttribute("data-img-srcs").split(";");
+
+    const cartoonName = cartoonDirectory.split("_").join(" ");
+
+    const modalBackground: HTMLElement =
+      document.getElementById("cartoon-display-bg");
+    const modalContent: HTMLElement = document.getElementById(
+      "cartoon-modal-content"
+    );
+    const title = modalBackground.querySelector("h1");
+
+    title.innerText = cartoonName;
+    modalContent.innerHTML = "";
+
+    for (const source of imageSources) {
+      const image = document.createElement("img");
+      image.src = `/assets/img/cartoons/uploads/${subject}/${courseCode}/${cartoonDirectory}/${source}`;
+      image.alt = `Cartoon about ${cartoonName}`;
+      modalContent.appendChild(image);
+    }
+
+    modalBackground.classList.add("active");
+  }
+}
+
+CartoonsArchive.init();
--- server/api/index.ts
+++ server/api/index.ts
@@ -4,10 +4,12 @@ import { ReadWriteAPIController } from "./controllers/read-write-api-controller"
 import express from "express";
 import { ExamBankController } from "./controllers/exam-bank-controller";
 import { StudentMiddleware } from "../auth/adfs";
+import { CartoonsController } from "./controllers/cartoons-controller";
 
 const router = express.Router();
 
 ExamBankController.rewriteFile();
+CartoonsController.rewriteFile();
 
 router.get("/data", (req: Request, res: Response) => {
   if (typeof req.query.path === "string") {
@@ -19,6 +21,20 @@ router.get("/data", (req: Request, res: Response) => {
 
 router.get("/exams", StudentMiddleware, (_req: Request, res: Response) => {
   ReadWriteAPIController.getJSONDataPath("_hidden/exams-list", res);
+});
+
+router.get("/cartoons", (_req: Request, res: Response) => {
+  ReadWriteAPIController.getJSONDataPath("_hidden/cartoons-list", res);
+});
+
+router.post("/exams/rebuild", (_req: Request, res: Response) => {
+  ExamBankController.rewriteFile();
+  res.status(201).send();
+});
+
+router.post("/cartoons/rebuild", (_req: Request, res: Response) => {
+  CartoonsController.rewriteFile();
+  res.status(201).send();
 });
 
 router.post("/general-inquiries", (req: Request, res: Response) => {
